c7f2a72a918fdae67285ec960ef5709704e47c7f
phwt.smwt <phwt.smwt@gmail.com>
2022-03-16 23:19:21 +0700

    feat: artist autocomplete

--- src/components/sections/ArtistsSection.tsx
+++ src/components/sections/ArtistsSection.tsx
@@ -1,20 +1,99 @@
-import { Typography, Button } from "@mui/material";
+import {
+    Typography,
+    Autocomplete,
+    TextField,
+    Grid,
+    Avatar,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSpotify } from "../../contexts/SpotifyContext";
 import withSection, { ISectionProps } from "../hocs/withSection";
 
 const ArtistsSection = ({ fullpageApi }: ISectionProps) => {
+    const [value, setValue] = useState<SpotifyApi.ArtistObjectFull | null>(
+        null
+    );
+    const [options, setOptions] = useState<
+        readonly SpotifyApi.ArtistObjectFull[]
+    >([]);
+    const [inputValue, setInputValue] = useState("");
+    const spotify = useSpotify();
+
+    useEffect(() => {
+        if (inputValue === "") {
+            setOptions(value ? [value] : []);
+            return undefined;
+        }
+
+        (async () => {
+            const artists = await spotify.searchArtists(inputValue, {
+                limit: 10,
+            });
+            setOptions(artists.artists.items);
+        })();
+    }, [value, inputValue, fetch]);
+
+    useEffect(() => {
+        if (value) fullpageApi.moveTo(3, 0); // To albums section
+    }, [value]);
+
     return (
         <>
             <Typography variant="h4" component="div" gutterBottom>
                 Select artist
             </Typography>
-            <Button
-                variant="contained"
-                onClick={() => {
-                    fullpageApi.moveTo(3, 0); // To albums section
+            <Autocomplete
+                getOptionLabel={(option) => option.name}
+                filterOptions={(x) => x}
+                options={options}
+                autoComplete
+                includeInputInList
+                filterSelectedOptions
+                value={value}
+                onChange={(
+                    event: any,
+                    newValue: SpotifyApi.ArtistObjectFull | null
+                ) => {
+                    setOptions(newValue ? [newValue, ...options] : options);
+                    setValue(newValue);
                 }}
-            >
-                Next
-            </Button>
+                onInputChange={(event, newInputValue) => {
+                    setInputValue(newInputValue);
+                }}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Select an artist"
+                        fullWidth
+                        sx={{ minWidth: 300 }}
+                    />
+                )}
+                renderOption={(props, option) => {
+                    return (
+                        <li {...props}>
+                            <Grid container alignItems="center">
+                                <Grid item>
+                                    <Avatar
+                                        variant="square"
+                                        src={option.images[0]?.url}
+                                        alt={option.name}
+                                    />
+                                </Grid>
+                                <Grid item xs>
+                                    {option.name}
+                                    <Typography
+                                        variant="body2"
+                                        color="text.secondary"
+                                    >
+                                        {option.followers.total.toLocaleString()}{" "}
+                                        followers
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </li>
+                    );
+                }}
+            />
         </>
     );
 };
