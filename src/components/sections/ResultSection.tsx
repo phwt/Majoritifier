a5e429150d22f4d5b20eceab70ea832bf44b0985
phwt.smwt <phwt.smwt@gmail.com>
2022-03-17 13:22:51 +0700

    feat: show result and playlist generation

--- src/components/sections/ResultSection.tsx
+++ src/components/sections/ResultSection.tsx
@@ -1,12 +1,81 @@
-import { Typography, Button } from "@mui/material";
+import {
+    Typography,
+    Button,
+    List,
+    ListItem,
+    ListItemText,
+} from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+import { useAuthentication } from "../../contexts/AuthenticationContext";
+import { useForm } from "../../contexts/FormContext";
+import { useSpotify } from "../../contexts/SpotifyContext";
 import withSection, { ISectionProps } from "../hocs/withSection";
 
 const AlbumsSection = ({ fullpageApi }: ISectionProps) => {
+    const { albums } = useForm();
+    const spotify = useSpotify();
+    const { user } = useAuthentication();
+
+    const [results, setResults] = useState<SpotifyApi.TrackObjectFull[]>([]);
+
+    useEffect(() => {
+        // TODO: Run only once when form is submitted
+        (async () => {
+            setResults([]);
+
+            const albumTracks = (
+                await Promise.all(
+                    albums.map(async (album) => {
+                        return (
+                            await spotify.getAlbumTracks(album.id)
+                        ).items.map((t) => t.id);
+                    })
+                )
+            ).flat();
+
+            const tracks = (await spotify.getTracks(albumTracks)).tracks;
+            setResults([...tracks].sort((i, j) => j.popularity - i.popularity));
+        })();
+    }, [albums]);
+
+    const savePlaylist = useCallback(async () => {
+        const playlist = await spotify.createPlaylist(user?.id ?? "", {
+            name: "Generated Playlist",
+            public: false,
+        });
+        await spotify.addTracksToPlaylist(
+            playlist.id,
+            results.slice(0, 10).map((t) => t.uri)
+        );
+    }, [results]);
+
     return (
         <>
             <Typography variant="h4" component="div" gutterBottom>
                 Your playlist
             </Typography>
+            <List
+                sx={{
+                    overflowY: "auto",
+                    maxHeight: "80vh",
+                }}
+            >
+                {results.map((track) => (
+                    <ListItem key={track.id}>
+                        <ListItemText>
+                            {track.name} - {track.popularity}
+                        </ListItemText>
+                    </ListItem>
+                ))}
+            </List>
+            <Button
+                variant="contained"
+                onClick={() => {
+                    savePlaylist();
+                }}
+            >
+                Save as playlist
+            </Button>
             <Button
                 variant="contained"
                 onClick={() => {
